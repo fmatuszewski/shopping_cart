5adcce4a2b7b4093fd57b1ad29ee502ccf52a46f
Filip Matuszewski <filip.matuszewski@gmail.com>
2019-11-30 13:29:24 +0100

    initial commit

---
+++ lib/pages/bucket.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BucketWidget extends StatefulWidget {
+  @override
+  _BucketWidgetState createState() => _BucketWidgetState();
+}
+
+class _BucketWidgetState extends State<BucketWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/product_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProductListWidget extends StatefulWidget {
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
