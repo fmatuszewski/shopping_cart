0dc3317fe280f6ad14106a10cafced6b433937c6
Filip Matuszewski <filip.matuszewski@gmail.com>
2019-11-30 13:55:04 +0100

    Dodanie listy produktow

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/product.dart';
 
 class ProductListWidget extends StatefulWidget {
   @override
@@ -6,8 +7,30 @@ class ProductListWidget extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductListWidget> {
+
+  List<Product> products;
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    products = [
+      Product("buty",9,""),
+      Product("skarpety",9,""),
+      Product("kuszla",9,"")
+
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView.builder(itemBuilder: (context, index)=>_buildProductRow(products[index]))
+    );
+  }
+
+  _buildProductRow(Product product) {
+
   }
 }
